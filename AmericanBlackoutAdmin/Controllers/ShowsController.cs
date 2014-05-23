9adbc0eca07df84c9b8593d4aefcecfe8b457bf4
Ross Dougherty <rossd987@gmail.com>
2014-05-22 20:18:11 -0600

    show and venue stuff

---
+++ AmericanBlackoutAdmin/Controllers/ShowsController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AmericanBlackout.Domain;
+using ServiceStack.Redis;
+
+namespace AmericanBlackoutAdmin.Controllers
+{
+    public class ShowsController : Controller
+    {
+        //
+        // GET: /Shows/
+        public ActionResult Index()
+        {
+            using (var redis = new RedisClient("nyu"))
+            {
+
+                var showsclient = redis.As<Show>();
+
+                //var band = new Band
+                //{
+                //    Id = bandclient.GetNextSequence(),
+                //    Name = "American Blackout",
+                //    Facebook = "AmericanBlackoutBand",
+                //    Website = "http://www.americanblackoutband.com"
+                //};
+
+                var shows = showsclient.GetAll();// .Lists["bands"];
+                return View(shows);
+            }
+        }
+
+        //
+        // GET: /Shows/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Shows/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Shows/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Shows/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Shows/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Shows/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Shows/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AmericanBlackoutAdmin/Controllers/VenueController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AmericanBlackout.Domain;
+using ServiceStack.Redis;
+
+namespace AmericanBlackoutAdmin.Controllers
+{
+    public class VenuesController : Controller
+    {
+        //
+        // GET: /Venue/
+        public ActionResult Index()
+        {
+            using (var redis = new RedisClient("nyu"))
+            {
+
+                var venueclient = redis.As<Venue>();
+
+                var venue = venueclient.GetAll();// .Lists["bands"];
+                return View(venue);
+            }
+        }
+
+        //
+        // GET: /Venue/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Venue/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Venue/Create
+        [HttpPost]
+        public ActionResult Create(Venue collection)
+        {
+            try
+            {
+                using (var redis = new RedisClient("nyu"))
+                {
+                    var venueclient = redis.As<Venue>();
+
+                    collection.Id = venueclient.GetNextSequence();
+
+                    venueclient.Store(collection);// .Lists[""]..Store(band);
+
+
+                    return RedirectToAction("Index");
+                }
+
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Venue/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Venue/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Venue/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Venue/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
