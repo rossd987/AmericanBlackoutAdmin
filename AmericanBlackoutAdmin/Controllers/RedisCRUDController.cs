0610969951a49a8075c90e68ff6e5ee5b9468655
Ross Dougherty <rossd987@gmail.com>
2014-07-15 20:54:50 -0600

    Controller refactoring

---
+++ AmericanBlackoutAdmin/Controllers/RedisCRUDController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AmericanBlackout.Domain.Redis;
+
+namespace AmericanBlackoutAdmin.Controllers
+{
+    public class RedisCRUDController<T> : Controller where T:RedisItem
+    {
+        public RedisCRUDController(IABRedisClient client)
+        {
+            _client = client;
+        }
+        protected readonly IABRedisClient _client;
+
+        public ActionResult Index()
+        {
+            using (var redis = _client.Create())
+            {
+                return View(redis.As<T>().GetAll());
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(T venue)
+        {
+            try
+            {
+                _client.Create(venue);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(_client.Get<T>(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, T venue)
+        {
+            try
+            {
+                _client.Store(venue);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(_client.Get<T>(id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, T venue)
+        {
+            try
+            {
+                _client.Delete(venue);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+	}
+}
