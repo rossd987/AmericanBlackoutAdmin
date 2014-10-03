cc881bc739bc1da4393d0e52ac5fb6780c4439e1
Ross Dougherty <rossd987@gmail.com>
2014-10-02 20:41:50 -0600

    add news items

---
+++ AmericanBlackoutAdmin/Controllers/NewsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using   AmericanBlackout.Domain;
+using AmericanBlackout.Domain.Redis;
+
+namespace AmericanBlackoutAdmin.Controllers
+{
+    public class NewsController : RedisCRUDController<NewsItem>
+    {
+        public NewsController(IABRedisClient client)
+            : base(client)
+        {
+        }
+	}
+}
