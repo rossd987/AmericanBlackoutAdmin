beb10d4f0892afd93270644cf2f4b38e634894c9
Ross Dougherty <rossd987@gmail.com>
2015-03-03 18:52:27 -0700

    some work

---
+++ AmericanBlackoutAdmin/Models/MusicEventModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using AmericanBlackout.Domain.SchemaOrg;
+
+namespace AmericanBlackoutAdmin.Models
+{
+    public class MusicEventModel 
+    {
+        public MusicEvent MusicEvent { get; set; }
+        public string Offers { get; set; }
+        public string Performers { get; set; }
+
+    }
+}
